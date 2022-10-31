06cff2722eb4efa3203bddd4f2757d5c8c055195
edlial <edlizaganjori@hotmail.com>
2022-10-31 09:55:03 +0100

    check version before updating

--- Installed_apps.ps1
+++ Installed_apps.ps1
@@ -1,6 +1,6 @@
 $latest_version = "1.1"
 $penta_path = "C:\Program Files\5Q"
-$info_json = (Get-Content "$penta_path\info.json" -Raw) | ConvertFrom-Json
+$info_json = (Get-Content "$penta_path\Installed_apps_info.json" -Raw) | ConvertFrom-Json
 $local_version = $info_json.psobject.Properties.Where({ $_.Name -eq "script_version" }).Value
 
 if ($local_version -eq $latest_version) {
@@ -8,56 +8,48 @@ if ($local_version -eq $latest_version) {
 }
 else {
     Write-Host "Applying Updates !"
-#update json file locally
-$jsonVar = @"
+    #update json file locally
+    $jsonVar = @"
 {
     "script_name": "Installed_apps",
     "script_version": "$latest_version"
 }
 "@
     
-    If(!(test-path -PathType container $penta_path))
-    {
+    If (!(test-path -PathType container $penta_path)) {
         New-Item -ItemType Directory -Path $penta_path
     }
 
-    $jsonVar | Out-File "$penta_path\info.json"
-} 
+    $jsonVar | Out-File "$penta_path\Installed_apps_info.json"
 
-# $LogFolder = "$penta_path"
-# If (Test-Path $LogFolder) {
-#     Write-Output "$LogFolder exists. Skipping."
-# }
-# Else {
-#     Write-Output "The folder '$LogFolder' doesn't exist. This folder will be used for storing logs created after the script runs. Creating now."
-#     Start-Sleep 1
-#     New-Item -Path "$LogFolder" -ItemType Directory
-#     Write-Output "The folder $LogFolder was successfully created."
-# }
+    Write-Host "======================================="
+    Write-Host "---       Start Managing Apps       ---"
+    Write-Host "======================================="
 
-# Start-Transcript -OutputDirectory "$LogFolder"
+    # Start-Transcript -OutputDirectory "$penta_path"
 
-# #Install choco if it's not installed
-# Get-PackageProvider -Name "Chocolatey" -ForceBootstrap
+    # #Install choco if it's not installed
+    # Get-PackageProvider -Name "Chocolatey" -ForceBootstrap
 
-# $to_install = @('7zip','notepadplusplus','git') #Shto adobe
-# $to_remove_choco = @('puppet-agent','googlechrome') 
-# $to_remove_winget = @('Puppet.puppet-agent','Google.Chrome')
+    # $to_install = @('7zip', 'notepadplusplus', 'git') #Shto adobe
+    # $to_remove_choco = @('puppet-agent', 'googlechrome') 
+    # $to_remove_winget = @('Puppet.puppet-agent', 'Google.Chrome')
 
-# foreach ($package in $to_install) {
-#     Write-Host "Installing $package."
-#     choco install -y $package
-# }
+    # foreach ($package in $to_install) {
+    #     Write-Host "Installing $package."
+    #     choco install -y $package
+    # }
 
-# foreach ($package in $to_remove_winget) {
-#     Write-Host "Winget is trying to remove $package."
-#     winget uninstall -h $package
-# }
+    # foreach ($package in $to_remove_winget) {
+    #     Write-Host "Winget is trying to remove $package."
+    #     winget uninstall -h $package
+    # }
 
-# foreach ($package in $to_remove_choco) {
-#     Write-Host "Choco is trying to remove $package."
-#     choco uninstall -y $package
-# }
+    # foreach ($package in $to_remove_choco) {
+    #     Write-Host "Choco is trying to remove $package."
+    #     choco uninstall -y $package
+    # }
 
-# choco upgrade all
-# winget upgrade --all -h
+    # choco upgrade all
+    # winget upgrade --all -h
+}
